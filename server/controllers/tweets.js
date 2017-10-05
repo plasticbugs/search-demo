42d90657da8b0d82133b655dcf881664d0d68c69
Scott Moschella <scottmosch@gmail.com>
2017-10-05 15:00:26 -0700

    Add search function to query db

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -1,11 +1,17 @@
 const Tweet = require('../models/tweet');
+const moment = require('moment');
 
-module.exports.search = (string, callback) => {
-  Tweet.find( { $text: { $search: "@nytopinion" } }, (err, result) => {
+module.exports.search = (req, res) => {
+  Tweet.find( { $text: { $search: req.query.q } } ).lean().exec( ( err, result ) => {
     if(err) {
-      callback(err)
+      console.log("There was a database error: ", err);
+      res.status(500).send({ error: 'Something went wrong.' });
     } else {
-      callback(null, result);
+      let formattedResult = result.map( tweet => {
+        tweet.formatted_date = moment(tweet.created_at, "YYYYMMDD").fromNow();
+        return tweet;
+      });
+      res.send(formattedResult);
     }
-  } )
+  });
 }
