a85a3efd25983c64fed9d30f0d5d4bc6c873a1a2
Scott Moschella <scottmosch@gmail.com>
2017-10-05 15:02:04 -0700

    Add query logic to App, render tweets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import Tweet from './Tweet.jsx';
+import Search from './Search.jsx';
+import 'normalize.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hello: "world"
+      searchResults: [],
+      loading: false
     }
+    this.handleSearch = this.handleSearch.bind(this);
   }
+
+  handleSearch(query) {
+    this.setState({loading: true});
+    axios.get('/api/search', {
+      params: {
+        q: query
+      }
+    })
+    .catch( err => {
+      this.setState({loading: false});
+      console.log('Error retrieving search results.')
+    })
+    .then( result => {
+      console.log(result.data)
+      this.setState( { searchResults: result.data, loading: false } );
+    })
+  }
+
   render() {
-  return (<div>{this.state.hello}</div>);
+    let results;
+    if(this.state.loading) {
+      results = <div>Loading...</div>;
+    } else {
+      results = (<ul>
+          { this.state.searchResults.map( tweet => {
+            console.log(tweet);
+            return (
+              < Tweet
+                user_id={ tweet.user_id }
+                text={ tweet.text }
+                date={ tweet.formatted_date }
+                handleSearch={ this.handleSearch }
+                key={ tweet._id }
+              />);
+            })
+          }
+        </ul>);
+    }
+    return (<div>
+      < Search handleSearch={ this.handleSearch } />
+      { results }
+    </div>);
   }
 }
 
 module.exports = App;
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('main')
+  < App />,
+  document.getElementById('root')
 );
