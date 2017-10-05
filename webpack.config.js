465eb4b363335f0dc14365837d84fc66df5d3535
Scott Moschella <scottmosch@gmail.com>
2017-10-04 22:57:42 -0700

    Create webpack configured for babel/react

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./src/components/App.jsx",
+  output: {
+    filename: "public/scripts/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  }
+};
