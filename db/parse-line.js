e2438bf97ac37ab4f3e118f1b2c026fe84147089
Scott Moschella <scottmosch@gmail.com>
2017-10-06 14:50:02 -0700

    Add constants for magic numbers

--- db/parse-line.js
+++ db/parse-line.js
@@ -1,10 +1,15 @@
 var Tweet = require('../server/models/tweet');
 const removeAccents = require('remove-accents');
 
+const DATE_END = 19;
+const TEXT_START = 20;
+const TEXT_END = 161;
+const USERNAME_START = 161;
+
 module.exports = function(line) {
-  let created_at = new Date(line.slice(0,19));
-  let user_id = line.slice(161, line.length).trim();
-  let text = line.slice(20, 161).trim();
+  let created_at = new Date(line.slice(0, DATE_END));
+  let user_id = line.slice(USERNAME_START, line.length).trim();
+  let text = line.slice(TEXT_START, TEXT_END).trim();
   let searchableText = removeAccents(text);
   
   return {
