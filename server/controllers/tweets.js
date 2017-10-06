3a3a3442cd18361f3f2818a1c1d14b61ffcfb7d3
Scott Moschella <scottmosch@gmail.com>
2017-10-06 14:50:25 -0700

    Move logic into controller

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -1,23 +1,14 @@
 const Tweet = require('../models/tweet');
-const moment = require('moment');
 const removeAccents = require('remove-accents');
 
 module.exports.search = (req, res) => {
   let sanitizedSearch = removeAccents(req.query.q);
-  Tweet.find( { $text: { $search: sanitizedSearch, $diacriticSensitive: false } }, {score : { $meta: "textScore" } } )
-    .sort( { score: { $meta: "textScore" } } )
-    .lean()
-    .exec( ( err, result ) => {
+  Tweet.search(sanitizedSearch, (err, results) => {
     if(err) {
       console.log("There was a database error: ", err);
       res.status(500).send({ error: 'Something went wrong.' });
     } else {
-      let formattedResult = result.map( tweet => {
-        tweet.formatted_date = moment(tweet.created_at, "YYYYMMDD").fromNow();
-        delete tweet.searchableText;
-        return tweet;
-      });
-      res.send(formattedResult);
+      res.send(results);
     }
   });
 }
