ebdb3e0a31ce13bf2025535b79d784dd43f24a64
Scott Moschella <scottmosch@gmail.com>
2017-10-06 15:08:05 -0700

    Clean up quotes

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -5,7 +5,7 @@ module.exports.search = (req, res) => {
   let sanitizedSearch = removeAccents(req.query.q);
   Tweet.search(sanitizedSearch, (err, results) => {
     if(err) {
-      console.log("There was a database error: ", err);
+      console.log('There was a database error: ', err);
       res.status(500).send({ error: 'Something went wrong.' });
     } else {
       res.send(results);
