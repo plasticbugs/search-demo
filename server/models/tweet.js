c50e22d8b16e6aff4a6bb42a8e60de4217c46e49
Scott Moschella <scottmosch@gmail.com>
2017-10-06 14:51:01 -0700

    Move logic into controller

--- server/models/tweet.js
+++ server/models/tweet.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const moment = require('moment');
 
 var tweetSchema = new Schema({
   created_at: Date,
@@ -7,4 +8,26 @@ var tweetSchema = new Schema({
   user_id: String
 });
 
-module.exports = mongoose.model('Tweet', tweetSchema);
+const Tweet = mongoose.model('Tweet', tweetSchema);
+
+module.exports = Tweet;
+
+module.exports.search = (query, callback) => {
+  console.log(query);
+  Tweet.find( { $text: { $search: query, $diacriticSensitive: false } }, {score : { $meta: "textScore" } } )
+  .sort( { score: { $meta: "textScore" } } )
+  .lean()
+  .exec( ( err, result ) => {
+    if(err) {
+      callback(err);
+    } else {
+      console.log(result.length);
+      let formattedResult = result.map( tweet => {
+        tweet.formatted_date = moment(tweet.created_at, "YYYYMMDD").fromNow();
+        delete tweet.searchableText;
+        return tweet;
+      });
+      callback(null, formattedResult);
+    }
+  });
+}
