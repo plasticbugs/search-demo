e9601ddd610f7e6b8d8d58da15d9aebc52e4d07b
Scott Moschella <scottmosch@gmail.com>
2017-10-06 09:59:44 -0700

    Remove accents from text to make searchable

--- db/seed_helpers.js
+++ db/seed_helpers.js
@@ -1,13 +1,16 @@
 var Tweet = require('../server/models/tweet');
+const removeAccents = require('remove-accents');
 
 module.exports = function(line) {
   let created_at = new Date(line.slice(0,19));
   let user_id = line.slice(161, line.length).trim();
   let text = line.slice(20, 161).trim();
+  let searchableText = removeAccents(text);
   
   return {
     created_at,
     user_id,
-    text
+    text,
+    searchableText
   }
 }
